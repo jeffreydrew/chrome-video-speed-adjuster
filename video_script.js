ada14c74742e57cc175c9c8098a146ae7c912195
Jeffrey Drew <98627285+jeffreydrew@users.noreply.github.com>
2023-01-11 23:57:36 -0500

    Michael - Re uploaded files for most recent update
    Michael here,
    Re uploaded the manifest.json and video_script.js for the most recent update, will learn to do this automatically later.

--- video_script.js
+++ video_script.js
@@ -1,21 +1,42 @@
-console.log("Test video script 6");
+console.log("Video Speed Adjuster Running...");
 
 document.addEventListener(
-  "keydown",
-  (event) => {
-    if (event.ctrlKey) {
-        if (event.altKey) {
-            if (event.key == "l" || event.key == "L") {
-                console.log("Ctrl + L pressed");
-                let videos = document.getElementsByTagName("video");
-                for (var i = 0; i < videos.length; i++) {
-                    videos[i].playbackRate = 2.5;
-                    console.log(videos[i]);
-                }
+    "keydown",
+    (event) => {
+        if (event.key == "d" || event.key == "D"){
+          console.log("D pressed, video speed + 0.5");
+          let videos = document.getElementsByTagName("video");
+          for (var i = 0; i < videos.length; i++) {
+            if (videos[i].playbackRate < 16){
+              videos[i].playbackRate += 0.5;
             }
+            else{
+              videos[i].playbackRate += 0;
+            }
+            console.log(videos[i] + ": " + videos[i].playbackRate);
+          }
         }
-    }
-  }, false);
+        if (event.key == "s" || event.key == "S") {
+          console.log("S pressed, video speed - 0.5");
+          let videos = document.getElementsByTagName("video");
+          for (var i = 0; i < videos.length; i++) {
+            if (videos[i].playbackRate != 0) {
+              videos[i].playbackRate -= 0.5;
+            } else {
+              videos[i].playbackRate -= 0;
+            }
+            console.log(videos[i] + ": " + videos[i].playbackRate);
+          }
+        }
+        if (event.key == "r" || event.key == "R") {
+          console.log("R pressed, video speed reset");
+          let videos = document.getElementsByTagName("video");
+          for (var i = 0; i < videos.length; i++) {
+            videos[i].playbackRate = 1;
+            console.log(videos[i] + ": " + videos[i].playbackRate);
+          }
+        }
+    }, false);
 
 // let videos = document.getElementsByTagName("video");
 // for (var i = 0; i < videos.length; i++) {
